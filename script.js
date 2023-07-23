42048a830cc0ddf12e4b990d885047ff89aba76f
AshrafS786 <ashrafali786522002@gmail.com>
2023-07-23 18:19:33 +0530

    First

---
+++ script.js
@@ -0,0 +1,34 @@
+gsap.to(".page1 svg",{
+    scale:0.12,
+    marginTop:"-125px",
+    scrollTrigger:{
+        trigger:".page1",
+        scroller:"body",
+        // markers:true,
+        start:"top -5%",
+        end:"top -10%",
+        scrub:3,
+    }
+})
+gsap.to(".page1 .page1-img",{
+    marginTop:"300px",
+    scrollTrigger:{
+        trigger:"#page1",
+        scroller:"body",
+        // markers:true,
+        start:"top -5%",
+        end:"top -10%",
+    },
+})
+
+gsap.to(".page1 .nav h5",{
+    visibility: "visible",
+    scrollTrigger:{
+        trigger:"#page1",
+        scroller:"body",
+        // markers:true,
+        start:"top -5%",
+        end:"top -10%",
+        scrub:true
+    },
+})
